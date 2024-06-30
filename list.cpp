8383ebbf37ecf84ff0be746699c105e54868fd94
ananya-aggarwalq <137142908+ananya-aggarwalq@users.noreply.github.com>
2024-06-30 21:38:25 +0530

    Update list.cpp

--- list.cpp
+++ list.cpp
@@ -2,6 +2,7 @@
 #include<stdexcept>
 #include "node.h"
 
+//constructor
 List::List(){
     sentinel_head = new Node(true);
     sentinel_tail = new Node(true);
@@ -11,6 +12,8 @@ List::List(){
     sentinel_tail->next = nullptr;
     size = 0;
 }
+
+//destructor
 List::~List(){
     while(sentinel_head->next != sentinel_tail){
         Node* n = sentinel_head->next;
@@ -20,12 +23,16 @@ List::~List(){
     delete sentinel_head;
     delete sentinel_tail;
 }
+
+//insertion
 void List::insert(int v){
     Node* n1 = new Node(v, sentinel_tail, sentinel_tail->prev) ;
     sentinel_tail->prev->next = n1;
     sentinel_tail->prev = n1;
     ++size;
 }
+
+//deletion
 int List::delete_tail(){
     if(size ==0){
         throw std::runtime_error("Empty Stack");
@@ -41,9 +48,13 @@ int List::delete_tail(){
         return value;
     }
 }
+
+//size
 int List::get_size(){
     return size;
 }
+
+//head
 Node* List::get_head(){
     return sentinel_head;
 }
