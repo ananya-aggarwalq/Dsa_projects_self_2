00bb042f5dd435be653050e1df3eedbe3db7c9b2
ananya-aggarwalq <137142908+ananya-aggarwalq@users.noreply.github.com>
2024-06-30 20:51:28 +0530

    Create list.cpp

---
+++ list.cpp
@@ -0,0 +1,49 @@
+#include "list.h"
+#include<stdexcept>
+#include "node.h"
+
+List::List(){
+    sentinel_head = new Node(true);
+    sentinel_tail = new Node(true);
+    sentinel_head->prev = nullptr;
+    sentinel_head->next = sentinel_tail;
+    sentinel_tail->prev = sentinel_head;
+    sentinel_tail->next = nullptr;
+    size = 0;
+}
+List::~List(){
+    while(sentinel_head->next != sentinel_tail){
+        Node* n = sentinel_head->next;
+        sentinel_head->next = n->next;
+        delete n;
+    }
+    delete sentinel_head;
+    delete sentinel_tail;
+}
+void List::insert(int v){
+    Node* n1 = new Node(v, sentinel_tail, sentinel_tail->prev) ;
+    sentinel_tail->prev->next = n1;
+    sentinel_tail->prev = n1;
+    ++size;
+}
+int List::delete_tail(){
+    if(size ==0){
+        throw std::runtime_error("Empty Stack");
+    }
+    else{
+        Node* tn = sentinel_tail->prev;
+        int value = tn->get_value();
+        tn->prev->next = sentinel_tail;
+        sentinel_tail->prev = tn->prev;
+
+        delete tn;
+        --size;
+        return value;
+    }
+}
+int List::get_size(){
+    return size;
+}
+Node* List::get_head(){
+    return sentinel_head;
+}
